fd3a6fb13050d651febefbb8b0b6ed4ac0765563
katotetsu <tech@katotetsu.net>
2024-09-29 09:28:57 +0900

    EnterのPush回数及びカウントダウンまで実装

--- pushentergame/app/page.tsx
+++ pushentergame/app/page.tsx
@@ -1,4 +1,12 @@
+"use client";
 import {useState,useEffect} from 'react';
+import{db}from '../firebase';
+import{collection,addDoc,getDocs,query,orderBy,limit} from 'firebase/firestore';
+
+type ScoreData ={
+  score:number;
+  timestamp:Date;
+};
 
 export default function Home() {
   //状態管理のためのuseState
@@ -6,6 +14,10 @@ export default function Home() {
   const[timeLeft,setTimeLeft]=useState(10);//ゲーム時間
   const[gameStarted,setGameStarted] = useState(false);//ゲームの状態
   const[isGameOver,setIsGameOver] = useState(false);//ゲームの終了状態
+  const[ranking,setRanking]=useState<ScoreData[]>([]);//ランキングデータ
+
+  //FireStoreのコレクションリファレンス
+  const scoresCollectionRef = collection(db,'scores');
 
   // ゲームのスタート
   const startGame = () => {
@@ -21,10 +33,44 @@ export default function Home() {
           clearInterval(timer); // タイマーの停止
           setIsGameOver(true); // ゲームオーバー
           setGameStarted(false);
+          saveScore();//ゲーム終了時にスコアを保存
         }
         return prevTime - 1;
       });
     }, 1000);
+  };
+
+  //スコアの保存
+  const saveScore = async () => {
+    try{
+      //FireStoreにスコアを保存
+      await addDoc(scoresCollectionRef,{score:count,timestam:new Date()});
+      console.log('Score saved successfully!');
+      fetchRanking();
+    }
+    catch(error){
+      console.error('Error saving score:',error);
+    }
+  };
+
+  //FireStoreからランキングを取得
+  const fetchRanking = async() =>{
+    try{
+      //スコアの高い順に上位10件を取得
+      const q=query(scoresCollectionRef,orderBy('score','desc'),limit(10));
+      const querySnapshot = await getDocs(q);
+      //ドキュメントデータを'scoreData'型にキャスト
+      const rankingData = querySnapshot.docs.map((doc)=>{
+        return{
+          score:doc.data().score as number,
+          //Firestoreのtimestamp型をJSのDate型に変換
+          timestamp:doc.data().timestamp.toDate() as Date,
+        };
+      });
+      setRanking(rankingData);
+    }catch(error){
+      console.error('Error fetching ranking',error);
+    }
   };
 
   // エンターキーの連打カウント
@@ -49,6 +95,14 @@ export default function Home() {
       <h2>Time Left: {timeLeft} seconds</h2>
       <h2>Enter Count: {count}</h2>
       {isGameOver && <h3>Game Over!</h3>}
+
+      {/* ランキングの表示 */}
+      <h3>Top Scores:</h3>
+      <ul>
+        {ranking.map((item, index) => (
+          <li key={index}>Score: {item.score}</li>
+        ))}
+      </ul>
     </div>
   );
 
